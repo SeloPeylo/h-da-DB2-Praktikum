f9e755ce3223ee075e9a50234f7c011f93bf79f9
struvanl <ruben.laack@stud.h-da.de>
2019-06-14 21:09:19 +0200

    Bug fix

--- docs/03/select_player_and_games.sql
+++ docs/03/select_player_and_games.sql
@@ -1,4 +1,4 @@
--- Player & Games
-SELECT player.id, player.name, game.id, game.enddatetime, game.maxquestions, game.startdatetime
+-- Select Player & Games (Game Data)
+SELECT player.id AS player_id, player.name, game.id AS game_id, game.enddatetime, game.maxquestions, game.startdatetime
 FROM master_data_knowledge_test.player INNER JOIN master_data_knowledge_test.game ON player.id = game.player_id
 ORDER BY player.id ASC, game.id ASC;
