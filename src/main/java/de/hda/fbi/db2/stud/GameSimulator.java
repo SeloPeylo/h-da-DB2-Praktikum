eca4991ac5503c6128f6252f31bd30ed21dcf711
struvanl <ruben.laack@stud.h-da.de>
2019-06-14 10:55:15 +0200

    Created class for symulation

---
+++ src/main/java/de/hda/fbi/db2/stud/GameSimulator.java
@@ -0,0 +1,33 @@
+package de.hda.fbi.db2.stud;
+
+import de.hda.fbi.db2.stud.entity.Player;
+
+public class GameSimulator {
+
+    private int countPlayer;
+    private int countGamesEach;
+
+
+    public GameSimulator(int countPlayer, int countGamesEach){
+        this.countPlayer = countPlayer;
+        this.countGamesEach = countGamesEach;
+    }
+
+    public void runSimulation(){
+
+    }
+
+    private void genPlayer(){
+        // random name of new player = count
+
+    }
+
+    private void genGame(Player player){
+        // count questions
+        // choose Categories count? 2-5?
+        // get questions / make guess / count right answers
+        // save count of right answers in game
+        // save game
+    }
+
+}
